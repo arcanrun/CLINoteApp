e1556e8032b0470715a8d7cd155fee2b177d879f
arcanrun <arcan.run@gmail.com>
2018-10-10 14:13:30 +0300

    notify class -ended

--- model/facade/subsystem/NotifyUbuntu.py
+++ model/facade/subsystem/NotifyUbuntu.py
@@ -1,24 +1,42 @@
 from model.facade.interfaces.INotifier import INotifier
+from model.facade.Facade import Facade
+from model.facade.subsystem.ShelveDb import ShelveDb
+
 import subprocess
-# from model.facade.subsystem.ShelveDb import ShelveDb
 import shelve
+import datetime
 
 
 class NotifyUbuntu(INotifier):
     def __init__(self):
-        pass
-    def notify(self):
-        db = shelve.open('ShelveDb')
+        db = ShelveDb()
+        self.facade = Facade(db)
 
-        for k,v in db:
-            notification = v.get_notification
-            if (not notification == None) and (not notification[1] == 1):
-                #  notify-send -a NoteCLI -i /home/arcan/PycharmProjects/CLINoteApp/icon.png "Title" "text"
-                message = '-a NoteCLI -i /home/arcan/PycharmProjects/CLINoteApp/icon.png "Title" "text"'
-                subprocess.Popen(['notify-send', message])
+    def create(self):
+        text = 'aloha!'
+        today = datetime.date.today()
+        category = 'Category:Fun'
+        title = '3rd note!'
+        self.facade.create_note(text, today, category, title)
+        self.facade.add_notify('1', datetime.date(2015, 10, 10))
+        print(self.facade.get_note_by_id('1').get_notification())
+    def notify(self):
+        db = shelve.open('../../../shelveDb')
+
+        for k,v in db.items():
+            notification = v.get_notification()
+            print(k,notification)
+
+            if (not notification == None) and (not notification[1] == 1) and (notification[0] <= datetime.date.today()):
+                title = v.get_title()
+                category = v.get_category()
+                text = '==' + category + '==\n' + v.get_text()
+
+                subprocess.Popen(['notify-send', '-i', '/home/arcan/PycharmProjects/CLINoteApp/icon.png', title, text])
         db.close()
         return
 
 if __name__ == '__main__':
     a = NotifyUbuntu()
+    # a.create()
     print(a.notify())
